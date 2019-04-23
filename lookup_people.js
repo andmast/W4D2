de52b774987af63320a76ec4bc288ecbe1d905fc
Andrea <anmastrantoni@gmail.com>
2019-04-23 17:12:29 +0000

    Refactor code

--- lookup_people.js
+++ lookup_people.js
@@ -3,7 +3,7 @@ const input = process.argv[2];
 
 db.findByName(input,function(err, output) {
       if(!err) {
-        console.log(`Found ${output.rows.length} track(s) by the name '${input}': `)
+        console.log(`Found ${output.rows.length} people by the name '${input}': `)
         output.rows.forEach( (element, index) => {
           console.log(`- ${index + 1}: ${element.first_name} ${element.last_name}, born '${element.birthdate.toLocaleDateString('en-CA')}'`);
         });
