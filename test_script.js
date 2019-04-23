d0df068d6b438a41c6ff97970602a21d9517d086
Andrea <anmastrantoni@gmail.com>
2019-04-23 17:09:54 +0000

    findbyname function implemented

---
+++ test_script.js
@@ -0,0 +1,35 @@
+module.exports = (function(){
+
+  const pg = require('pg');
+  const settings = require("./settings");
+
+  const client = new pg.Client({
+    user : settings.user,
+    password : settings.password,
+    database: settings.database,
+    host : settings.host,
+    port : settings.port,
+    ssl : settings.ssl
+  });
+
+  client.connect((err) => {
+    if (err) {
+      return console.error("Connection Error", err);
+    }
+  });
+
+
+  function findByName(name, callback) {
+    client.query(`SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1`, [name])
+    .then (results => {
+      callback(null, results)
+      client.end();
+    })
+    .catch (e => callback(e.stack))
+  }
+
+  return {
+    findByName: findByName
+  }
+
+})()
